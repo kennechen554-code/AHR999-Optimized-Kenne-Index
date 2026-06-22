6cbadd05416eb02905077e84310816daad1d41d8
Kennechen <kennechen554@gmail.com>
2026-06-23 00:01:59 +0800

    refactor(frontend): 删除未使用信号 Hook

--- frontend/src/hooks/useSignals.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from '@tanstack/react-query'
-import { fetchSignals } from '../services/api'
-
-export function useSignals() {
-  return useQuery({
-    queryKey: ['signals'],
-    queryFn: fetchSignals,
-  })
-}
